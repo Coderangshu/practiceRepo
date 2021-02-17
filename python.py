3830e72cb3b2dfe3217a9601a448bf8d81c79b80
Angshuman Sengupta <senguptaangshuman17@gmail.com>
2021-02-17 11:44:12 +0530

    python.py modified

--- python.py
+++ python.py
@@ -2,6 +2,8 @@ print("helllo world")
 print("this is it")
 print("trying out authentication and saving files, saving successful")
 def add(a,b):
+    a += a+1
+    b += b+1
     return a+b
 
-print(add(3,4))
+print(add(5,5))
